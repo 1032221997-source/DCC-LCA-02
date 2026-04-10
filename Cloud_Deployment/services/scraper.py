5c00d3add56fdce71b7497bd6cbbea19a578e210
Tanisha Chakraborty <tanisha.chakraborty@IPM-H9PQ4952DX.local>
2026-04-10 12:20:16 +0530

    Added SSL

--- Cloud_Deployment/services/scraper.py
+++ Cloud_Deployment/services/scraper.py
@@ -1,12 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
-
+import certifi
 def scrape_website(url: str) -> dict:
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36",
         "Accept-Language": "en-US,en;q=0.9"
     }
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, verify=certifi.where())
     response.raise_for_status()
     soup = BeautifulSoup(response.content, 'html.parser')
      
